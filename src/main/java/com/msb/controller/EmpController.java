1d3f369fa7b7bd49eb43aab7cdd5ccd3e9acb275
liuzhitao <193129139@qq.com>
2024-06-04 12:56:25 +0800

    使用了三层架构进行了拆分

--- src/main/java/com/msb/controller/EmpController.java
+++ src/main/java/com/msb/controller/EmpController.java
@@ -1,5 +1,7 @@
 package com.msb.controller;
 
+import com.msb.Service.EmpService;
+import com.msb.Service.impl.EmpServiceA;
 import com.msb.pojo.Emp;
 import com.msb.pojo.Result;
 import com.msb.utils.XmlParserUtils;
@@ -16,35 +18,15 @@ import java.util.List;
  */
 @RestController
 public class EmpController {
+    private EmpService empService = new EmpServiceA();
+
     @RequestMapping("/listEmp")
     public Result list(){
-//        1\ 加载并解析emp.xml
-        String file = this.getClass().getClassLoader().getResource("emp.xml").getFile();
+        List<Emp> daoList = empService.getDaoList();
 
-        List<Emp> emps = XmlParserUtils.parse(file, Emp.class);
-        System.out.println(emps);
-
-//        2\ 对数据进行转换处理
-        emps.stream().forEach(emp -> {
-            String gender = emp.getGender();
-            if("1".equals(gender)){
-                emp.setGender("男");
-            }else if("2".equals(gender)){
-                emp.setGender("女");
-            }
-
-            String job = emp.getJob();
-            if("1".equals(job)){
-                emp.setJob("学生");
-            }else if("2".equals(job)){
-                emp.setJob("老师");
-            }else if("3".equals(job)){
-                emp.setJob("其他人员");
-            }
-        });
 
 //        3、 响应数据
-        return Result.success(emps);
+        return Result.success(daoList);
 
 
     }
